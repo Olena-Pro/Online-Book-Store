39dd4f6deec3eead54f9bc5256811d241a7b77fc
Olena-Pro <128047343+Olena-Pro@users.noreply.github.com>
2024-03-03 18:36:04 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Book;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can`t save movie - " + book + " to BD", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> getAllCinemaHallQuery = session.createQuery("from Book",
+                    Book.class);
+            return getAllCinemaHallQuery.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get a bok list", e);
+        }
+    }
+}
+
+
